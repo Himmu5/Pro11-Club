b9c053e42806f713ebb05c01747dd2fcfa83c4e7
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-07-24 09:34:53 +0530

    Nav bar added

--- src/Components/UI-Component/Nav.tsx
+++ src/Components/UI-Component/Nav.tsx
@@ -1,24 +1,73 @@
-import { FC } from "react";
-import Hamburger from 'hamburger-react'
-import Logo from '../../../public/logo.png'
+import { Box, Button, Dialog, Typography } from "@mui/material";
+import { FC, useState } from "react";
+import Logo from "../../../public/logo.png";
+import Hamburger from "hamburger-react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { motion } from "framer-motion";
 
 type P = object;
 
 const Nav: FC<P> = () => {
+  const matches = useMediaQuery("(min-width:600px)");
+  const [open, setOpen] = useState(false);
+  const toggleHam = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className="flex justify-between items-center p-2 ">
-      <div className="h-10 w-10 ">
-        <img src={Logo} className=" h-full w-full " alt="logo" />
-      </div>
+    <Box
+      display={"flex"}
+      justifyContent={"space-between"}
+      padding={2}
+      fontStyle={"primary"}
+      position={"relative"}
+      alignItems={"center"}
+    >
+      <img src={Logo} style={{ height: 30, width: 40 }} alt="logo" />
+      <Typography variant="h6" children="Pro 11 Club" />
+      <motion.div hidden={matches} className="relative z-10" onTap={toggleHam}>
+        <Hamburger size={20} toggled={open} />
+      </motion.div>
+      
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={toggleHam}
+        style={{ backgroundColor: "#E4E4E4" }}
+        className="text-primary"
+      >
+        <Box className="flex justify-between" padding={2} display={"flex"}>
+          <div></div>
+          <div className="flex gap-2 items-center">
+            <img src={Logo} style={{ height: 30, width: 40 }} alt="logo" />
+            <Typography variant="h6" children="Pro 11 Club" />
+          </div>
+          <div>
+            <motion.div
+              hidden={matches}
+              className="relative z-10"
+              onTap={toggleHam}
+            >
+              <Hamburger size={20} toggled={open} />
+            </motion.div>
+          </div>
+        </Box>
 
-      <div className="text-xl font-bold ">
-        <h1>Pro 11 Club</h1>
-      </div>
-
-      <div className="md:invisible">
-        <Hamburger />
-      </div>
-    </div>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          gap={5}
+          marginTop={10}
+          alignItems={"center"}
+        >
+          <Typography variant="h5" children="Home" />
+          <Typography variant="h5" children="FAQ's" />
+          <Typography variant="h5" children="About Us" />
+          <Typography variant="h5" children="Contact Us" />
+          <Button children="Download App" size="large" variant="contained" />
+        </Box>
+      </Dialog>
+    </Box>
   );
 };
 export default Nav;
