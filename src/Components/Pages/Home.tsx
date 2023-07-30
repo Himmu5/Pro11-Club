2f5791083fa0d453117f0ad6e1e0b3805b8ef748
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-07-30 12:08:20 +0530

    Home page Responsive added

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,17 +1,26 @@
 import { Button } from "@mui/material";
 import { FC } from "react";
 import PhoneImage from "../UI-Component/PhoneImage";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import useMediaQuery from "@mui/material/useMediaQuery";
+
 type P = object;
 const Home: FC<P> = () => {
+  const matches = useMediaQuery("(min-width : 600px)");
+
   return (
-    <div className="flex flex-col p-2 text-center gap-3">
-      <div className="flex flex-col text-3xl font-outline font-bold " > <p>Dominate The Fantasy</p> <p>Cricket Arena.</p> </div>
-      <p>
-        Indulge in captivating fantasy cricket. Build your dream team,
-        strategize, and compete in leagues. Pro 11 Club awaits!
-      </p>
-      <Button children="DOWNLOAD APP" variant="contained" sx={{ marginLeft: 10, marginRight: 10 }} />
-      <PhoneImage />
+    <div className="flex flex-col p-2 text-center gap-1 sm:flex-row items-center sm:text-left sm:max-w-7xl sm:mx-auto sm:mx-4">
+      <div className="flex flex-col gap-3 sm:w-1/2 sm:pt-6">
+        <div className="flex flex-col text-3xl md:text-4xl sm:max-w-xl font-outline font-bold " > <p>Dominate The Fantasy</p> <p>Cricket Arena.</p> </div>
+        <p className="text-sm">
+          Indulge in captivating fantasy cricket. Build your dream team,
+          strategize, and compete in leagues. Pro 11 Club awaits!
+        </p>
+        <Button children="DOWNLOAD APP" endIcon={<ArrowForwardIcon />} variant="contained" sx={{ paddingLeft: 1, paddingRight: 2, alignSelf: matches === true ? "start" : "center" }} />
+      </div>
+      <div className="sm:w-1/2">
+        <PhoneImage />
+      </div>
     </div>
   );
 };
--- src/Components/UI-Component/PhoneImage.tsx
+++ src/Components/UI-Component/PhoneImage.tsx
@@ -4,9 +4,9 @@ import Ring from '../../../public/Phones.png'
 
 type P = object
 const PhoneImage: FC<P> = () => {
-    return <div className='  my-10 w-full'>
-        <img src={Phones} alt="phones" className='  absolute z-2 -rotate-3' />
-        <img src={Ring} alt="ring" className='relative -bottom-12 ' />
+    return <div className='  py-10 '>
+        <img src={Phones} alt="phones" className=' sm:w-1/2 sm:-my-10 absolute z-2  -rotate-3  ' />
+        <img src={Ring} alt="ring" className=' relative -bottom-12 sm:w-full' />
     </div>
 }
 export default PhoneImage;
