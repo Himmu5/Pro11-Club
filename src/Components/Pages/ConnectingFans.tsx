7f08621b1f475752409d097c18898a4cb1604184
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-01 21:27:04 +0530

    Connecting fans page UI fix

--- src/Components/Pages/ConnectingFans.tsx
+++ src/Components/Pages/ConnectingFans.tsx
@@ -4,13 +4,15 @@ import ThreePhones from '../../../public/ThreePhones.svg'
 type P = object
 
 const ConnectingFans: FC<P> = () => {
-    return <div className='flex flex-col text-center '>
-        
-        <h1 className='text-3xl font-bold '>Real-time Banter Connecting Fans Amplifying Excitement.</h1>
-        <p className='text-sm space-y-3 py-5 flex flex-col'><span>Engage in lively conversations with fellow fans,</span> <span>sharing the exhilaration of live matches through Pro 11</span><span> Club's immersive chat experience.</span></p>
-        <div className='relative'></div>
-        <img src={ThreePhones} alt="Three Phones" className=' relative py-10 px-4' />
-
+    return <div className=' flex flex-col sm:flex-row text-center sm:text-start py-28 lg:py-64 sm:p-2'>
+        <div className='sm:w-2/3 md:space-y-5'>
+            <h1 className='text-3xl font-bold sm:text-4xl md:text-6xl'>Real-time Banter Connecting Fans Amplifying Excitement.</h1>
+            <p className='text-sm space-y-3 py-5 flex flex-col lg:text-2xl'><span>Engage in lively conversations with fellow fans,</span> <span>sharing the exhilaration of live matches through Pro 11</span><span> Club's immersive chat experience.</span></p>
+            <div className='relative'></div>
+        </div>
+        <div className='sm:w-1/3'>
+            <img src={ThreePhones} alt="Three Phones" className='sm:w-full py-10 px-4' />
+        </div>
     </div>
-}   
+}
 export default ConnectingFans;
