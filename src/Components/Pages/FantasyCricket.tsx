f10386030823a2e97144ec291bd04ba2130589ed
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-01 20:42:47 +0530

    Fantasy Cricket responsiveness fix

--- src/Components/Pages/FantasyCricket.tsx
+++ src/Components/Pages/FantasyCricket.tsx
@@ -6,12 +6,12 @@ type P = object
 
 const FantasyCricket: FC<P> = () => {
     return <div className=' flex flex-col '>
-        <div className='flex flex-col sm:w-full sm:items-center '>
-            <img src={TwoPhones} alt="TwoPhones" className='absolute sm:relative z-10 mb-20 sm:mb-0  sm:w-4/6  ' />
-            <h1 className='text-4xl mt-[210px] sm:mt-0 top-0 font-bold text-center sm:text-7xl relative  sm:w-full  '>CRICKET FANTASY</h1>
+        <div className='flex flex-col sm:w-full sm:items-center relative'>
+            <img src={TwoPhones} alt="TwoPhones" className=' sm:-mt-40  mb-20 sm:mb-0  sm:w-4/6  relative z-10' />
+            <h1 className='text-4xl font-bold text-center sm:text-7xl md:text-9xl absolute top-72 sm:w-full  '>CRICKET FANTASY</h1>
         </div>
 
-        <img src={CutBall} alt="CutBall" className='h-56 self-end pt-10 ' />
+        <img src={CutBall} alt="CutBall" className=' h-56 self-end pt-10 ' />
     </div>
 }
 export default FantasyCricket;
