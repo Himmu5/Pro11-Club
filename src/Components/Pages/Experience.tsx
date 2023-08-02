279e2480a1ca7a227e6fc4b441bb55411acc1e08
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-02 08:37:04 +0530

    Experience page fixes

--- src/Components/Pages/Experience.tsx
+++ src/Components/Pages/Experience.tsx
@@ -11,16 +11,16 @@ type P = object
 const Experience: FC<P> = () => {
     return <>
         <div className='sm:flex sm:items-center max-w-7xl mx-auto'>
-            <div className='flex items-center justify-between sm:w-full'>
-                <div className='pt-10 '>
+            <div className='flex items-center justify-between sm:w-1/2'>
+                <div className='pt-10 w-2/3 '>
                     <img src={Ball} alt='Ball' className=' absolute sm:flex left-56 mt-24 ' />
                     <img src={Player} alt="Player" className='relative  ' />
                 </div>
-                <div className=' sm:hidden mt-8 mr-6'>
+                <div className=' sm:invisible sm:w-0 mt-8 mr-6'>
                     <img src={Star} alt="Star" />
                 </div>
             </div>
-            <h1 className='text-2xl text-center mt-8 text-[#111111] sm:text-5xl font-bold '>Unleash the amazing Gaming Experience</h1>
+            <h1 className='text-3xl text-center sm:text-left mt-8 text-[#111111] sm:text-5xl font-bold flex flex-col '><span> Unleash the amazing </span><span>Gaming Experience</span></h1>
         </div>
         <div className='sm:flex sm:items-center  sm:justify-center max-w-7xl mx-auto'>
             <div className='flex w-full '>
