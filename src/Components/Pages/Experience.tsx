6f789fe8dcbfcb738ba77756735e8b56952e78ac
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-03 14:39:56 +0530

    Animation UI changes

--- src/Components/Pages/Experience.tsx
+++ src/Components/Pages/Experience.tsx
@@ -12,13 +12,14 @@ import { motion } from 'framer-motion'
 type P = object
 
 const Experience: FC<P> = () => {
-    const size = useMediaQuery("(min-width : 600px)");
+    const sm = useMediaQuery("(min-width : 600px)");
+    const md = useMediaQuery("(min-width : 1080px)");
     
     return <>
         <div className='sm:flex sm:items-center max-w-7xl mx-auto'>
             <div className='flex items-center justify-between sm:w-1/2'>
                 <div className='pt-10 w-2/3 '>
-                    <motion.div whileInView={{ opacity: 1, x : -200 , y : 70  ,  transition : { duration : 1.5 } } } ><motion.img src={Ball} alt='Ball' className=' absolute sm:flex left-96 top-6 sm:top-0 -mr-96 ' /></motion.div>
+                    <motion.div whileInView={{ opacity: 1, x : md === true ? -80 :-200 , y :  md === true  ? 140 :70  ,  transition : { duration : 1.5 } } } ><motion.img src={Ball} alt='Ball' className=' absolute sm:flex left-96 -mr-96 ' /></motion.div>
                     <img src={Player} alt="Player" className='relative  ' />
                 </div>
                 <div className=' sm:invisible sm:w-0 mt-8 mr-6 '>
@@ -29,7 +30,7 @@ const Experience: FC<P> = () => {
         </div>
         <div className='sm:flex sm:items-center  sm:justify-center max-w-7xl mx-auto'>
             <div className='flex w-full '>
-                {size === true ? <ResponsiveExperienceMapper /> : <ExperienceMapper />}
+                {sm === true ? <ResponsiveExperienceMapper /> : <ExperienceMapper />}
             </div>
             <div className='sm:w-2/3 sm:h-full'>
                 <img src={EPhone} alt="EPhone" />
