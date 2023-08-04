fb32cebec2199bba0ae802bddece136b7ac6ac5e
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-04 14:50:20 +0530

    minor changes

--- src/Components/Pages/ElevateExperience.tsx
+++ src/Components/Pages/ElevateExperience.tsx
@@ -7,18 +7,18 @@ import Fade from 'react-reveal/Fade';
 type P = object
 
 const ElevateExperience: FC<P> = () => {
-    return <Fade bottom>
-        <div className='text-center flex flex-col sm:flex-row-reverse items-center sm:text-start py-10 px-2 sm:w-full'>
-            <img src={Star1} alt="Star1" className=' self-start py-4 ' />
-            <div className='flex flex-col gap-5 sm:w-2/3  '>
-                <h1 className='flex flex-col w-full  '> <span className='text-3xl sm:text-4xl font-bold md:text-5xl '> Elevate Your </span><span className='text-2xl font-bold sm:text-5xl md:text-6xl w-full '> Cricket Fantasy Experience</span> </h1>
-                <p className='md:text-2xl'>Take your cricket fantasy journey to new heights with Pro 11 Club, where excitement knows no bounds.</p>
-            </div>
-            <div className='flex flex-col relative  sm:flex-row sm:w-1/3'>
-                <img src={Star2} alt="Star2" className='self-end absolute  -mr-6 ' />
-                <img src={Ball} className=' h-64 mt-8 sm:h-full sm:w-full' alt="Ball" />
-            </div>
+    return <div className='text-center flex flex-col sm:flex-row-reverse items-center sm:text-start py-10 px-2 sm:w-full'>
+        <img src={Star1} alt="Star1" className=' self-start py-4 ' />
+        <div className='flex flex-col gap-5 sm:w-2/3  '>
+            <h1 className='flex flex-col w-full  '> <span className='text-3xl sm:text-4xl font-bold md:text-5xl '> Elevate Your </span><span className='text-2xl font-bold sm:text-5xl md:text-6xl w-full '> Cricket Fantasy Experience</span> </h1>
+            <p className='md:text-2xl'>Take your cricket fantasy journey to new heights with Pro 11 Club, where excitement knows no bounds.</p>
         </div>
-    </Fade>
+        <div className='flex flex-col relative  sm:flex-row sm:w-1/3'>
+            <img src={Star2} alt="Star2" className='self-end absolute  -mr-6 ' />
+            <Fade top>
+                <img src={Ball} className=' h-64 mt-8 sm:h-full sm:w-full' alt="Ball" />
+            </Fade>
+        </div>
+    </div>
 }
 export default ElevateExperience;
