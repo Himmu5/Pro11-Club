dece05bb2b21dbac75814cda85a9ef3af65ed889
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-04 07:55:05 +0530

    Witness Gaming revel effect added

--- src/Components/Pages/WitnessGaming.tsx
+++ src/Components/Pages/WitnessGaming.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import Star from '../../../public/Star.png'
 import Ball from '../../../public/CutBall.svg'
-
+import Fade from 'react-reveal/Fade';
 type P = object
 
 const WitnessGaming: FC<P> = () => {
@@ -17,18 +17,20 @@ const WitnessGaming: FC<P> = () => {
         "https://images.pexels.com/photos/16883535/pexels-photo-16883535/free-photo-of-woman-in-white-dress-posing-in-building-door.jpeg",
     ]
 
-    return <div className='mt-10 flex flex-col gap-4 items-center '>
-        <img src={Star} alt="Star" className='h-24 self-end' />
-        <h1 className='font-bold text-3xl sm:text-4xl md:text-7xl text-center sm:flex sm:flex-col '> <span> Witness the Power of</span><span> Mobile Fantasy Gaming!</span></h1>
-        <div className='flex gap-12 sm:gap-16 mt-10 w-full overflow-auto px-4 sm:z-10 sm:relative no-scrollbar '>
-            {
-                images.map((img) =><> <img alt='images' className=' h-32 w-40 sm:h-80 sm:w-[300px] object-cover rounded-xl shadow-md shadow-gray-400 ' src={img} /></>)
-            }
-        </div>
+    return <Fade bottom>
+        <div className='mt-10 flex flex-col gap-4 items-center '>
+            <img src={Star} alt="Star" className='h-24 self-end' />
+            <h1 className='font-bold text-3xl sm:text-4xl md:text-7xl text-center sm:flex sm:flex-col '> <span> Witness the Power of</span><span> Mobile Fantasy Gaming!</span></h1>
+            <div className='flex gap-12 sm:gap-16 mt-10 w-full overflow-auto px-4 sm:z-10 sm:relative no-scrollbar '>
+                {
+                    images.map((img) => <> <img alt='images' className=' h-32 w-40 sm:h-80 sm:w-[300px] object-cover rounded-xl shadow-md shadow-gray-400 ' src={img} /></>)
+                }
+            </div>
 
-        <div className='sm:flex sm:justify-center sm:mt-80 sm:absolute sm:w-full '>
-            <img src={Ball} alt="Ball" className='rotate-90 sm:w-1/3  ' />
+            <div className='sm:flex sm:justify-center sm:mt-80 sm:absolute sm:w-full '>
+                <img src={Ball} alt="Ball" className='rotate-90 sm:w-1/3  ' />
+            </div>
         </div>
-    </div>
+    </Fade>
 }
 export default WitnessGaming;
