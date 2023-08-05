a5e0e4830f73a480ec22d9bbcef19d2511fe9b74
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-05 16:24:03 +0530

    Path Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { Route, Routes } from "react-router-dom"
 import Footer from "./Components/Pages/Footer"
 import Main from "./Components/Pages/Main"
 import Nav from "./Components/UI-Component/Nav"
+import AboutUs from "./Components/Pages/AboutUs"
 
 function App() {
 
@@ -9,7 +11,10 @@ function App() {
 
       <Nav />
       <div className=" max-w-7xl mx-auto text-primary ">
-        <Main />
+        <Routes >
+          <Route element={<Main />} path="/Main" />
+          <Route element={<AboutUs />} path="/About" />
+        </Routes>
         <Footer />
       </div>
     </>
