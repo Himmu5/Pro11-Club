82124aee4b30999cc6890f9058cf54dae9bc3d86
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-05 16:17:36 +0530

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import AboutUs from "./Components/Pages/AboutUs"
-// import ConnectingFans from "./Components/Pages/ConnectingFans"
-// import ElevateExperience from "./Components/Pages/ElevateExperience"
-// import Experience from "./Components/Pages/Experience"
-// import FantasyCricket from "./Components/Pages/FantasyCricket"
-// import Footer from "./Components/Pages/Footer"
-// import Home from "./Components/Pages/Home"
-// import WitnessGaming from "./Components/Pages/WitnessGaming"
-// import Nav from "./Components/UI-Component/Nav"
+import ConnectingFans from "./Components/Pages/ConnectingFans"
+import ElevateExperience from "./Components/Pages/ElevateExperience"
+import Experience from "./Components/Pages/Experience"
+import FantasyCricket from "./Components/Pages/FantasyCricket"
+import Footer from "./Components/Pages/Footer"
+import Home from "./Components/Pages/Home"
+import WitnessGaming from "./Components/Pages/WitnessGaming"
+import Nav from "./Components/UI-Component/Nav"
 
 function App() {
 
   return (
     <>
-    <AboutUs />
-      {/* <Nav />
+    
+      <Nav />
       <div className=" max-w-7xl mx-auto text-primary ">
         <Home />
         <Experience />
@@ -22,7 +22,7 @@ function App() {
         <WitnessGaming />
         <ConnectingFans />
         <Footer />
-      </div> */}
+      </div>
     </>
   )
 }
