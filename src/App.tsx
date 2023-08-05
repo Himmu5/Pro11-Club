b089ac9e40bc95a5d8d608904af2ac33b6640f7e
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-05 20:20:45 +0530

    Privacy Policy added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { Route, Routes } from "react-router-dom"
-import Footer from "./Components/Pages/Footer"
-import Main from "./Components/Pages/Main"
-import Nav from "./Components/UI-Component/Nav"
-import AboutUs from "./Components/Pages/AboutUs"
+// import { Route, Routes } from "react-router-dom"
+// import Footer from "./Components/Pages/Footer"
+// import Main from "./Components/Pages/Main"
+// import Nav from "./Components/UI-Component/Nav"
+// import AboutUs from "./Components/Pages/AboutUs"
+
+import PrivacyPolicy from "./Components/Pages/PrivacyPolicy"
 
 function App() {
 
   return (
     <>
-   
-      <Nav />
+
+      <PrivacyPolicy />
+      {/* <Nav />
       <div className=" max-w-7xl mx-auto text-primary font-outline">
         <Routes >
           <Route element={<Main />} path="/" />
           <Route element={<AboutUs />} path="/About" />
         </Routes>
         <Footer />
-      </div>
+      </div> */}
     </>
   )
 }
