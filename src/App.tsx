15a97bf71d6db08f471af03313090d671f0bfef5
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-06 07:00:36 +0530

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-// import { Route, Routes } from "react-router-dom"
-// import Footer from "./Components/Pages/Footer"
-// import Main from "./Components/Pages/Main"
-// import Nav from "./Components/UI-Component/Nav"
+import { Route, Routes } from "react-router-dom"
+import Footer from "./Components/Pages/Footer"
+import Main from "./Components/Pages/Main"
+import Nav from "./Components/UI-Component/Nav"
 import AboutUs from "./Components/Pages/AboutUs"
 
 
@@ -9,16 +9,14 @@ function App() {
 
   return (
     <>
-    <AboutUs />
-
-      {/* <Nav />
+      <Nav />
       <div className=" max-w-7xl mx-auto text-primary font-outline">
         <Routes >
           <Route element={<Main />} path="/" />
           <Route element={<AboutUs />} path="/About" />
         </Routes>
         <Footer />
-      </div> */}
+      </div>
     </>
   )
 }
