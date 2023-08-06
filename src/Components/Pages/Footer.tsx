7b08f1cb5d8e0162e609f3bd9c0876840bde6d6d
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-06 18:54:04 +0530

    Footer changes

--- src/Components/Pages/Footer.tsx
+++ src/Components/Pages/Footer.tsx
@@ -6,8 +6,8 @@ import { AiFillFacebook, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai'
 
 type P = object
 const Footer: FC<P> = () => {
-    return <div  name="Footer">
-           
+    return <div name="Footer">
+
         <div className='flex flex-col items-center py-10 '>
             <div className='flex gap-4'>
                 <img src={Logo} alt="Logo" />
@@ -33,6 +33,18 @@ const Footer: FC<P> = () => {
                     <h1>About Us</h1>
                 </div>
             </div>
+
+        </div>
+        <div className='backdrop-blur-xl p-2'>
+
+            <h1 className='font-bold text-xl text-black '> CORPORATE OFFICE</h1>
+            <p>
+                ONE BKC, Tower A, 12th & 14th Floor, Unit 1201 & 1202 and 1401 & 1402, Plot C-66, G Block, Bandra Kurla Complex, Bandra (East), Mumbai 400 051
+            </p>
+            <p className='py-3'>
+                DISCLAIMER:The fantasy sport games on the BalleBaazi platform involve an element of financial risk and may be addictive. Please play responsibly and at your own risk. SBN Gaming and/or BalleBaazi platform does not promote or advertise fantasy sport games as an income opportunity or an alternative employment option. SBN Gaming and/or BalleBaazi platform shall not, in any way whatsoever, be held responsible and liable for your breach of any rules & regulations, terms & conditions and applicable laws by you. Learn More
+            </p>
+
         </div>
     </div>
 }
