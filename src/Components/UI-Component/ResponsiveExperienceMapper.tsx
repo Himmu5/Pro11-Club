47a0427cfd83161aaf22f1971cb09d16c1a6d4a8
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-07 09:49:35 +0530

    minor changes

--- src/Components/UI-Component/ResponsiveExperienceMapper.tsx
+++ src/Components/UI-Component/ResponsiveExperienceMapper.tsx
@@ -14,7 +14,7 @@ type P = {
 const ExperienceMapper: FC<P> = ({ setSelectedOption }) => {
 
     return <div className='flex flex-col gap-3 w-full h-[380px] overflow-auto custom-scrollbar'>
-        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5' onClick={() => setSelectedOption(1)}>
+        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5 cursor-pointer ' onClick={() => setSelectedOption(1)}>
             <div className='flex items-center gap-4'>
                 <img src={BlueTick} alt="BlueTick" />
                 <p>Earn Badges to Get Extra Reward</p>
@@ -22,7 +22,7 @@ const ExperienceMapper: FC<P> = ({ setSelectedOption }) => {
             <BiChevronRight />
         </div>
 
-        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5' onClick={() => setSelectedOption(2)}>
+        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5 cursor-pointer' onClick={() => setSelectedOption(2)}>
             <div className='flex items-center gap-4'>
                 <img src={Chat} alt="Chart" />
                 <p>Play Along Chat</p>
@@ -30,7 +30,7 @@ const ExperienceMapper: FC<P> = ({ setSelectedOption }) => {
             <BiChevronRight />
         </div>
 
-        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5' onClick={() => setSelectedOption(3)}>
+        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5 cursor-pointer' onClick={() => setSelectedOption(3)}>
             <div className='flex items-center gap-4'>
                 <img src={Trophie} alt="Chat" />
                 <p>Get Rewarded for being an Expert</p>
@@ -38,14 +38,14 @@ const ExperienceMapper: FC<P> = ({ setSelectedOption }) => {
             <BiChevronRight />
         </div>
 
-        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5' onClick={() => setSelectedOption(4)}>
+        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5 cursor-pointer' onClick={() => setSelectedOption(4)}>
             <div className='flex items-center gap-4'>
                 <img src={person} alt="person" />
                 <p>Get Expert’s help</p>
             </div>
             <BiChevronRight />
         </div>
-        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5' onClick={() => setSelectedOption(5)}>
+        <div className='flex justify-between items-center px-3 rounded-md py-4 shadow-lg  border mr-5 cursor-pointer' onClick={() => setSelectedOption(5)}>
             <div className='flex items-center gap-4'>
                 <img src={Chart} alt="person" />
                 <p>Lowest Entry Leagues</p>
