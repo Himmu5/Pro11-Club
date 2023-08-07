19f0649564685bc2d52fd85b631c95b6e4dc7831
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-07 12:44:45 +0530

    UI changes

--- src/Components/Pages/ConnectingFans.tsx
+++ src/Components/Pages/ConnectingFans.tsx
@@ -9,7 +9,7 @@ type P = object
 
 const ConnectingFans: FC<P> = () => {
     return <Fade bottom>
-        <div className=' flex flex-col sm:flex-row text-center sm:text-start py-28 lg:py-64 sm:p-2 '>
+        <div className=' flex flex-col sm:flex-row text-center sm:text-start lg:py-64 sm:p-2 '>
             <div className='sm:w-2/3 md:space-y-5'>
                 <h1 className='text-3xl font-bold sm:text-4xl md:text-6xl'>Real-time Banter Connecting Fans Amplifying Excitement.</h1>
                 <p className='text-sm space-y-3 py-5 flex flex-col lg:text-2xl'><span>Engage in lively conversations with fellow fans,</span> <span>sharing the exhilaration of live matches through Pro 11</span><span> Club's immersive chat experience.</span></p>
@@ -17,12 +17,12 @@ const ConnectingFans: FC<P> = () => {
                 <div className='sm:h-full'>
                     
                     <Roll top right duration={3000}>
-                        <img src={BigBall} alt="Big Ball" />
+                        <img src={BigBall} className='hidden md:block' alt="Big Ball" />
                     </Roll>
                 </div>
             </div>
-            <div className='sm:w-1/2 sm:h-full hidden sm:block'>
-                <img src={ThreePhones} alt="Three Phones" className=' sm:w-full py-10 px-4' />
+            <div className='sm:w-1/3 sm:h-full hidden sm:block'>
+                <img src={ThreePhones} alt="Three Phones" className=' sm:w-full pt-10 px-4' />
             </div>
         </div>
     </Fade>
--- src/Components/Pages/WitnessGaming.tsx
+++ src/Components/Pages/WitnessGaming.tsx
@@ -20,7 +20,7 @@ const WitnessGaming: FC<P> = () => {
     ]
 
     return <Fade bottom>
-        <div className='mt-10 flex flex-col gap-4 items-center min-h-screen '>
+        <div className='mt-10 flex flex-col gap-4 items-center sm:min-h-screen '>
             <img src={Star} alt="Star" className='h-24 self-end' />
             <h1 className='font-bold text-3xl sm:text-4xl md:text-6xl text-center sm:flex sm:flex-col '> <span> Witness the Power of</span><span> Mobile Fantasy Gaming!</span></h1>
             <div className='flex gap-12 sm:gap-16 mt-10 w-full overflow-auto px-4 sm:z-10 sm:relative no-scrollbar '>
