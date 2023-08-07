d2f88b99d63162433b99ddc8e4552e2f2e2fc7fd
Himanshu chauhan <Adityachauhan5566@gmail.com>
2023-08-07 07:20:53 +0530

    Nav bg change

--- src/Components/UI-Component/Nav.tsx
+++ src/Components/UI-Component/Nav.tsx
@@ -5,20 +5,20 @@ import Hamburger from "hamburger-react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { motion } from "framer-motion";
 import { Link } from "react-scroll";
-import { Link as RouterLink } from 'react-router-dom' 
+import { Link as RouterLink } from 'react-router-dom'
 
 type P = object;
 
 const Nav: FC<P> = () => {
   const matches = useMediaQuery("(min-width : 600px)");
   const [open, setOpen] = useState(false);
-  
+
   const toggleHam = () => {
     setOpen(!open);
   };
 
   return (
-    <div className="sticky backdrop-blur-md md:backdrop-blur-none bg-transparent z-30 top-0 w-full ">
+    <div className="sticky backdrop-blur-md md:backdrop-blur-none  bg-transparent z-30 top-0 w-full ">
       <Box
         display={"flex"}
         justifyContent={"space-between"}
@@ -30,7 +30,7 @@ const Nav: FC<P> = () => {
         <RouterLink to={"/"} className="flex items-center gap-4 ">
           <img src={Logo} style={{ height: 25, width: 40 }} alt="logo" />
           <h1 className="font-bold text-xl" children="Pro 11 Club" />
-        </RouterLink> 
+        </RouterLink>
 
         <div className="sm:flex items-center gap-16 hidden font-bold text-primary text-xl cursor-pointer">
           <Link to="Home" smooth duration={500}>Home</Link>
@@ -38,7 +38,7 @@ const Nav: FC<P> = () => {
           <RouterLink to="/About">About Us</RouterLink>
           <p>Contact Us</p>
         </div>
-        <Button children="Download Now" variant="contained" sx={{ display: matches === false ? "none" : "flex" , borderRadius:0 }} style={{ backgroundColor: "#222229" }} className="bg-primary" />
+        <Button children="Download Now" variant="contained" sx={{ display: matches === false ? "none" : "flex", borderRadius: 0 }} style={{ backgroundColor: "#222229" }} className="bg-primary" />
         <motion.div hidden={matches} className="relative z-10" onTap={toggleHam}>
           <Hamburger size={20} toggled={open} />
         </motion.div>
@@ -48,39 +48,43 @@ const Nav: FC<P> = () => {
           fullScreen
           open={open}
           onClose={toggleHam}
-          style={ { backgroundColor: "#E4E4E4" }}
+          style={{ backgroundColor: "#E4E4E4" }}
         >
-          <Box className="flex justify-between" padding={2} display={"flex"}>
-            <div></div>
-            <div className="flex gap-2 items-center">
-              <img src={Logo} style={{ height: 30, width: 40 }} alt="logo" />
-              <Typography variant="h6" children="Pro 11 Club" />
-            </div>
-            <div>
-              <motion.div
-                hidden={matches}
-                className="relative z-10"
-                onTap={toggleHam}
-              >
-                <Hamburger size={20} toggled={open} />
-              </motion.div>
-            </div>
-          </Box>
+          <div className="bg-gray-200 h-full ">
 
-          <Box
-            display={"flex"}
-            fontWeight={20}
-            flexDirection={"column"}
-            gap={5}
-            marginTop={10}
-            alignItems={"center"}
-          >
-            <Typography variant="h4" children="Home" />
-            <Typography variant="h4" children="FAQ's" />
-            <Typography variant="h4" children="About Us" />
-            <Typography variant="h4" children="Contact Us" />
-            <Button children="Download App" size="large" variant="contained" style={{ backgroundColor: "#222229", borderRadius:0 }} />
-          </Box>
+            <Box className="flex justify-between" padding={2} display={"flex"}>
+              <div></div>
+              <div className="flex gap-2 items-center">
+                <img src={Logo} style={{ height: 30, width: 40 }} alt="logo" />
+                <Typography variant="h6" children="Pro 11 Club" />
+              </div>
+              <div>
+                <motion.div
+                  hidden={matches}
+                  className="relative z-10"
+                  onTap={toggleHam}
+                >
+                  <Hamburger size={20} toggled={open} />
+                </motion.div>
+              </div>
+            </Box>
+
+            <Box
+              display={"flex"}
+              fontWeight={20}
+              flexDirection={"column"}
+              gap={5}
+              marginTop={10}
+              alignItems={"center"}
+            >
+
+              <Typography variant="h4" children="Home" />
+              <Typography variant="h4" children="FAQ's" />
+              <Typography variant="h4" children="About Us" />
+              <Typography variant="h4" children="Contact Us" />
+              <Button children="Download App" size="large" variant="contained" style={{ backgroundColor: "#222229", borderRadius: 0 }} />
+            </Box>
+          </div>
         </Dialog>
         {/* </div> */}
       </Box>
